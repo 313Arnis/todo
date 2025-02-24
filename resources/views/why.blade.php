66457a41ff74c4013a21bed456c79aea8c1431b7
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2025-02-24 14:49:42 +0200

    kkas jauns

---
+++ resources/views/why.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Uzdevumu saraksts</h1>
+        <p>Mūsdienu straujajā dzīves ritmā efektīva laika plānošana un uzdevumu pārvaldība ir kļuvusi par būtisku ikdienas sastāvdaļu. Darāmo darbu saraksta vietne piedāvā strukturētu pieeju, kas palīdz lietotājiem organizēt savus pienākumus un sasniegt mērķus.</p>
+    <h2>Kāpēc darāmo darbu saraksts ir nepieciešams ikvienam?</h2>
+        <p>1. Uzdevumu organizēšana un prioritizēšana: Saraksts ļauj skaidri saskatīt visus veicamos uzdevumus, palīdzot noteikt to svarīgumu un steidzamību. Tas novērš haosu un palīdz koncentrēties uz būtiskāko.
+<br>2. Stresa samazināšana: Pierakstot uzdevumus, mēs atbrīvojam prātu no nepieciešamības tos atcerēties, tādējādi mazinot trauksmi un pārslodzi.
+<br>3. Produktivitātes paaugstināšana: Sistemātiska uzdevumu izpilde un to atzīmēšana kā paveikti sniedz gandarījumu un motivē turpināt darbu, palielinot efektivitāti.
+<br>4. Laika pārvaldība: Plānojot dienu ar darāmo darbu sarakstu, ir vieglāk sadalīt laiku starp dažādiem uzdevumiem, nodrošinot, ka nekas netiek aizmirsts un visi termiņi tiek ievēroti.
+<br>5. Līdzsvars starp darbu un privāto dzīvi: Saraksts palīdz nodalīt profesionālos pienākumus no personīgajiem, veicinot veselīgu darba un dzīves balansu.</p>
+</body>
+</html>
