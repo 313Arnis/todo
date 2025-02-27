cd2f2593834cdf5c86696668bd5111a640fef3ab
ipa23.a.razuvanovs <ipa23.a.razuvanovs@pjvt.local>
2025-02-27 15:14:52 +0200

    saku taisit diaries

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ToDoController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/why', function () {
     return view('why');
 });
+Route::get('/todos', [ToDoController::class, 'index']);
+Route::get('/diaries', [DiariesController::class, 'index']);
